dfef55bc3454102ff222305f6f501df9d295bfcb
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-03-08 11:03:25 +0100

    initial commit

---
+++ src/main/java/com/fontys/luc/butcherorder/ButcherorderApplication.java
@@ -0,0 +1,13 @@
+package com.fontys.luc.butcherorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButcherorderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fontys/luc/butcherorder/ButcherorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontys.luc.butcherorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcherorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
