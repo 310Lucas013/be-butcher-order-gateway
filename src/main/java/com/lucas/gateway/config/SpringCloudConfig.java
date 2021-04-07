7a7393cea9ff81228266c1be08be284eb71ea23a
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-04-07 10:48:29 +0200

    latest changes

--- src/main/java/com/lucas/gateway/config/SpringCloudConfig.java
+++ src/main/java/com/lucas/gateway/config/SpringCloudConfig.java
@@ -11,17 +11,17 @@ public class SpringCloudConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/credentials/**")
-                        .uri("http://credentials:8080/"))
+                        .uri("http://bo-credentials:8080/"))
                 .route(r -> r.path("/customer/**")
-                        .uri("http://customer:8080/"))
+                        .uri("http://bo-customer:8080/"))
                 .route(r -> r.path("/location/**")
-                        .uri("http://location:8080/"))
+                        .uri("http://bo-location:8080/"))
                 .route(r -> r.path("/butcher/**")
-                        .uri("http://butcher:8080/"))
+                        .uri("http://bo-butcher:8080/"))
                 .route(r -> r.path("/order/**")
-                        .uri("http://order:8080/"))
+                        .uri("http://bo-order:8080/"))
                 .route(r -> r.path("/product/**")
-                        .uri("http://product:8080/"))
+                        .uri("http://bo-product:8080/"))
                 .build();
     }
 }
